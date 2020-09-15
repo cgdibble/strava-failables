c26e575810306683fdc139b22fcb8cffd3524630
cgdibble <cgdibble@gmail.com>
2020-09-15 10:56:25 -0700

    setup tokenization correctly, it seems. couple util tests to get the token

--- src/strava.js
+++ src/strava.js
@@ -1,20 +1,32 @@
-const { success, failure } = require('@pheasantplucker/failables')
+const { success, failure } = require('@pheasantplucker/failables');
 const stravaApi = require('strava-v3');
 
-let stravaClient
+let stravaClient;
 
-const configureStrava = (accessToken) => {
-  if (!accessToken) return failure('No access token provided')
-  stravaClient = new stravaApi.client(accessToken)
-  return success(stravaClient)
-}
+const getToken = async () => {
+  const allAccess = 'read,read_all,profile:read_all,profile:write,activity:read,activity:read_all,activity:write';
+  const url = stravaApi.oauth.getRequestAccessURL({scope: allAccess});
+  return url;
+};
+
+const configureStrava = (config) => {
+  if (!config) return failure('No access token provided');
+  stravaClient = new stravaApi.config(config);
+  return success(stravaClient);
+};
 
 const getAthlete = async (athleteId) => {
-  const result = await stravaApi.athlete.get({id: athleteId})
-  return success(result)
-}
+  const result = await stravaApi.athlete.get({id: athleteId});
+  return success(result);
+};
 
+const getAllActivities = async (athleteId) => {
+  const result = await stravaApi.athlete.listActivities({id: athleteId});
+  return success(result);
+};
 module.exports = {
     configureStrava,
-    getAthlete
-}
+    getAthlete,
+    getAllActivities,
+    getToken
+};
--- src/strava.test.js
+++ src/strava.test.js
@@ -1,45 +1,75 @@
-const { assertSuccess, assertFailure, assertSuccessWhich } = require('@pheasantplucker/failables')
-const { configureStrava, getAthlete } = require('./strava')
-const {
-	STRAVA_ACCESS_TOKEN
-} = process.env
+const { assertSuccess, assertFailure, assertSuccessWhich } = require('@pheasantplucker/failables');
+const { configureStrava, getAthlete, getAllActivities, getToken } = require('./strava');
+const stravaApi = require('strava-v3');
 
+const {
+	STRAVA_SPECIFIC_ACCESS_TOKEN,
+	STRAVA_ACCESS_TOKEN,
+	STRAVA_CLIENT_ID,
+	STRAVA_CLIENT_SECRET
+} = process.env;
+
+const stravaConfig = {
+	'access_token': STRAVA_ACCESS_TOKEN,
+	'client_id': STRAVA_CLIENT_ID,
+	'client_secret': STRAVA_CLIENT_SECRET,
+	'redirect_uri': 'http://localhost:3000'
+};
 describe('strava.js', () => {
-	describe('getAccess()', () => {
+	// my athlete id
+	const athleteId = 14547931;
+
+	describe.skip('getToken()', () => {
+		// these two are used to get the access token to run the other requests
+		it('should get a valid token', async () => {
+			configureStrava(stravaConfig);
+			const result = await getToken();
+			console.log('result', result);
+		});
+
+		it('should things', async () => {
+			// use the url output from above and in the browser get access. The redirect to localhost will
+			// have the `code` in the URL. That code goes here as STRAVA_SPECIFIC_ACCESS_TOKEN
+			const payload = await stravaApi.oauth.getToken(STRAVA_SPECIFIC_ACCESS_TOKEN);
+			console.log('payload', payload);
+		});
+	});
+	
+	describe('configureStrava()', () => {
 		it('should fail when no access token provided', () => {
-			const result = configureStrava()
-			assertFailure(result)
-		})
+			const result = configureStrava();
+			assertFailure(result);
+		});
 
 		it('should create client when given an access token', () => {
-			const result = configureStrava(STRAVA_ACCESS_TOKEN)
-			assertSuccess(result)
-		})
-	})
+			const result = configureStrava(stravaConfig);
+			assertSuccess(result);
+		});
+	});
 	
 	describe('Athletes', () => {
 		beforeAll(() => {
-			configureStrava(STRAVA_ACCESS_TOKEN)
-		})
+			configureStrava(stravaConfig);
+		});
 
 		it('should get the athlete data', async () => {
-			// my athlete id
-			const athleteId = 14547931
 			const result = await getAthlete(athleteId);
-			assertSuccessWhich((athlete) => athlete.id = athleteId, result)
-		})
+			assertSuccessWhich((athlete) => athlete.id = athleteId, result);
+		});
 		// Skipped because Strava API seems to only return the one athelete regardless
 		// this is not ideal, but the goal is to get a tool to look at my data
 		// so punting that weirdness for now. This is likely because of my use of the access token.
+		// Confirmed: https://www.npmjs.com/package/strava-v3#overriding-the-default-access_token
 		it.skip('should fail if no valid access token', async () => {
-			const result = await getAthlete()
-			assertFailure(result)
-		})
-	})
+			const result = await getAthlete(1);
+			assertFailure(result);
+		});
+	});
 
 	describe('Activities', () => {
 		it('should get the activities', async () => {
-			
-		})
-	})
+			const result = await getAllActivities(athleteId);
+			assertSuccess(result);
+		});
+	});
 });
